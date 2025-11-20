1b1e81367f3048c821b60517d2708ea83ed5488b
my projects <aish.charlette@outlook.com>
2025-11-20 14:12:16 +0000

    Exercise.02-completed

---
+++ 02-simple-sums.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() { // defines main function
+    int Num1 = 8; // assigning value to variable num1  
+    int Num2 = 10; // assigning value to variable Num2
+    int Sum = Num1 + Num2; // adding values together
+
+    cout << Sum; // output statement of calculated sum
+
+    return 0;
+}
