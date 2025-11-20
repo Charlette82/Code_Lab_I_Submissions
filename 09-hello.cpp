afb7f49f8e4dcf0799297e632cf07d86b2a8d50c
my projects <aish.charlette@outlook.com>
2025-11-20 14:19:16 +0000

    Exercise-09-hello-completed

---
+++ 09-hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() { // Define main function
+    string message = "Hello: "; // Define variable to store message
+    cout << "Hello: " << endl; // output statement
+
+    return 0;
+}
