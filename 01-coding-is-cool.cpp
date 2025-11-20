85ae1cd8e08d94677113eede105cc6e14d8f0bac
my projects <aish.charlette@outlook.com>
2025-11-20 14:07:15 +0000

    01-coding-is-cool-exercisecomplete

---
+++ 01-coding-is-cool.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() { 
+     // define main function
+     cout << "coding is cool" <<  endl;
+     // output statement 
+
+     return 0;
+}
